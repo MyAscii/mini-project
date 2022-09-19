93efae8579286fcac28bfd3b384ec2dde05cc29a
MyAscii <0xInvestigator@protonmail.com>
2022-09-19 21:15:06 +0200

    del project1 and init p1_dice_roll

---
+++ p1_dice_roll.py
@@ -0,0 +1,12 @@
+import random
+
+dmin = 1
+dmax = 6
+
+print("The dice are rolling....")
+
+mydice = random.randint(dmin, dmax)
+print("My dice:",mydice)
+
+yourdice = random.randint(dmin, dmax)
+print("Your dice:",yourdice)
